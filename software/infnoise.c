61d534ef1de6992d1fe89aadeca7367ba93e2578
Manuel Domke <manuel-domke@gmx.org>
2018-04-16 18:39:15 +0200

    remove BUILD_DATE from --version

--- software/infnoise.c
+++ software/infnoise.c
@@ -367,7 +367,6 @@ int main(int argc, char **argv)
 	printf("GIT VERSION - %s\n", GIT_VERSION);
 	printf("GIT COMMIT  - %s\n", GIT_COMMIT);
 	printf("GIT DATE    - %s\n", GIT_DATE);
-	printf("BUILD DATE  - %s\n", BUILD_DATE);
 	return 0;
     }
 
